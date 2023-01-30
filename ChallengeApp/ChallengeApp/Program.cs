a353869485225e2475cc1477052d176bdfa9d49f
Markus007x <markus.pluta@yahoo.de>
2023-01-30 17:25:42 +0100

    Day 4

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, Markus!");
+﻿string name = "ewa";
+string gender = "woman";
+byte age = 31;
+
+if (age < 30 & gender == "woman")
+{
+    Console.WriteLine("Kobieta ponizej 30");
+}
+else if (name == "ewa" & age == 33 & gender == "woman")
+{
+    Console.WriteLine("Ewa, lat 33");
+}
+else if (age < 18 & gender == "man")
+{
+    Console.WriteLine("Nieponoletni Facet");
+}
+else
+{
+    Console.WriteLine("Jestes Ufoludek");
+}
