2c3edf6e2a8fbd1e3c52b85a2f9c678b0eab33ea
Markus007x <markus.pluta@yahoo.de>
2023-02-09 11:41:08 +0100

    Day 6 Part 2

---
+++ ChallengeApp/ChallengeApp/Employee.cs
@@ -0,0 +1,31 @@
+﻿namespace ChallengeApp
+{
+    public class Employee
+    {
+        
+        private List<int> score = new List<int>();
+
+        public Employee(string surname, string name, int age)
+        {
+            this.surname = surname;
+            this.name = name;
+            this.age = age;
+        }
+
+        public string surname { get; private set; }
+        public string name { get; private set; }
+        public int age { get; private set; }
+        public int Result
+        {
+            get
+            {
+                return this.score.Sum();
+            }
+        }
+
+        public void AddScore(int number)
+        {
+            this.score.Add(number);
+        }
+    }
+}
