ed02b96809444b9a93437af8f73dd1d6d3274ed3
Markus007x <markus.pluta@yahoo.de>
2023-02-23 09:06:40 +0100

    Employee Test Day 9

--- ChallengeApp/ChallengeApp.Test/EmployeeTest.cs
+++ ChallengeApp/ChallengeApp.Test/EmployeeTest.cs
@@ -2,23 +2,56 @@
 
 namespace ChallengeApp.Test
 {
-    public class Tests
+    public class EmployeeTest
     {
-       [Test]
-        public void CheckSumOperation()
+        [Test]
+        public void CheckMaxGrade()
         {
             // arrange  Preparation
-            var employe1 = new Employee("Adam","Kowalski",35);
-            employe1.AddScore(-5);
-            employe1.AddScore(-10);
+            var employee = new Employee("Adam", "Kowalski");
+            employee.AddGrade(1);
+            employee.AddGrade(2);
+            employee.AddGrade(7);
 
             // act      Execution
-            var result = employe1.Result;
+            var statistics = employee.GetStatistics();
 
-            
+
             // assert   Assumption whether successful
-            Assert.AreEqual(-15, result);    
+            Assert.AreEqual(7, statistics.Max);
         }
+        [Test]
+        public void CheckMinGrade()
+        {
+            // arrange  Preparation
+            var employee = new Employee("Adam", "Kowalski");
+            employee.AddGrade(1);
+            employee.AddGrade(2);
+            employee.AddGrade(7);
 
+            // act      Execution
+            var statistics = employee.GetStatistics();
+
+
+            // assert   Assumption whether successful
+            Assert.AreEqual(1, statistics.Min);
+        }
+        [Test]
+        public void CheckAverage()
+        {
+            // arrange  Preparation
+            var employee = new Employee("Adam", "Kowalski");
+            employee.AddGrade(1);
+            employee.AddGrade(2);
+            employee.AddGrade(7);
+
+            // act      Execution
+            var statistics = employee.GetStatistics();
+
+
+            // assert   Assumption whether successful
+            Assert.AreEqual(Math.Round(3.33, 2), Math.Round(statistics.Average, 2));
+        }
+        
     }
 }
